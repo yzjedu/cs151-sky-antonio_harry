cb04e602f8654d54cc1da5e2522f384f39353302
HarryLi2028 <Hli2@loyola.edu>
2024-10-01 20:29:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 # Due Date: 10/2/2024
 # Lab Assignment: Ski jump
 # Problem Statement:  Let’s make a program to calculate the distance traveled based on speed and determine how many points they’d receive if they went that distance.
+
 import math 
+
+#Output the purpose of the code/ program
 print('Winter is coming! Ski season is here! This program would calculate the ski distance traveled based on the speed and ramp size and would calculate how many points you would get for the travelled distance.')
+
+#Prompt the user to input the hill type, large or normal
 hill_type = str(input('Please enter the hill type large or normal:' ))
 if hill_type.lower() == 'normal':
     hill_height = 46
@@ -18,17 +23,24 @@ else:
     while hill_type != 'large' and hill_type != 'normal':
         hill_type = str(input('Please enter the correct hill type large or normal:' ))
 
+#Using the hill type calculate the air time using the formula
 air_time = math.sqrt((2*hill_height)/9.8) 
 
-meter_per_sec = float(input('Enter speed in meters per second:'))
-while meter_per_sec <= 0:
-    meter_per_sec = float(input('Please enter speed in meters per second above 0:'))
-distance_travelled = float(meter_per_sec*air_time)
+#Prompt user to input a speed in meters every second above 0 and store it in a variable named 'meters_per_sec'
+meters_per_sec = float(input('Enter speed in meters traveled per second:'))
+while meters_per_sec <= 0:
+    meters_per_sec = float(input('Please enter speed in meters per second above 0:'))
 
-total_points = float(60 + (distance_travelled - par) * points_per_meter)
+#Calculate distance traveled by multiplying meter_per_sec and air_time
+distance_traveled = float(meters_per_sec*air_time)
 
-print(f'You have travelled {distance_travelled:.2f} meters.')
+#Calculate the total amount of points using the formula and store it in a variable named 'total_points'
+total_points = float(60 + (distance_traveled - par) * points_per_meter)
 
+#Output the distance the user has traveled 
+print(f'You have traveled {distance_traveled:.2f} meters.')
+
+#Output certain response based on the points earned
 if total_points > 60:
     print(f'Great job you have earned {total_points:.0f} points. that is more than par')
 elif total_points <= 0:
